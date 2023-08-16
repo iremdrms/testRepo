68212eae0dffc1cbde7da8bf19a44bb6f4fe1e06
iremdrms <durmus.irem@outlook.com>
2023-08-16 14:20:56 +0300

    .

--- REACT TUTORIAL/reactapp/src/App.js
+++ REACT TUTORIAL/reactapp/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import User from "./components/User";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Navbar />
+      <hr />
+      <User name="İrem Durmuş" department="Yazılım" salary="10000" />
+
+      <User name="Ayşe Yılmaz" salary="7500" department="Bilişim" />
     </div>
   );
 }
